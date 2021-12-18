92161922057ff77d844395f18f0cb2cce1a9903e
nitin kumar <nitin.kumar@smu.ca>
2021-12-17 17:39:18 -0800

    citymovie selection controller and view

---
+++ TicketBooking/Models/CityMovie.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketBooking.Models
+{
+    public class CityMovie
+    {
+        public int Id { get; set; }
+        public string? MovieName { get; set; }
+        public string? MovieImagePath { get; set; }
+
+        public IEnumerable<CityMovie>? lstCityMovie { get;set; }
+    }
+}
---
+++ TicketBooking/Repository/CityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using TicketBooking.IRepository;
+using TicketBooking.Models;
+using Dapper;
+namespace TicketBooking.Repository
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly IDapperRepository dapperRepository;
+        public CityRepository(IDapperRepository iDapperRepository)
+        {
+            dapperRepository = iDapperRepository;
+        }
+        public IEnumerable<City> GetCities()
+        {
+            return dapperRepository.Execute<City>(new DapperProperty() { CommandType = CommandType.StoredProcedure, ProcName = "[dbo].[GetCityList]", DynamicParameters = null }).ToList();
+        }
+
+        public IEnumerable<CityMovie> GetCityMovie(int cityId)
+        {
+            var dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("@CityId", cityId);
+            return dapperRepository.Execute<CityMovie>(new DapperProperty() { CommandType = CommandType.StoredProcedure, ProcName = "[dbo].[GetMovieList]", DynamicParameters = dynamicParameters }).ToList();
+        }
+
+       
+    }
+}
