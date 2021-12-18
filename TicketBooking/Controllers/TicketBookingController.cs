5f6c574898bcf3d15cfce151df954d615365891f
nitin kumar <nitin.kumar@smu.ca>
2021-12-17 17:14:41 -0800

    city get controller

---
+++ TicketBooking/Controllers/TicketBookingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketBooking.IRepository;
+using TicketBooking.Models;
+
+namespace TicketBooking.Controllers
+{
+    public class TicketBookingController : Controller
+    {
+        private readonly ICityRepository cityRepository;
+        public TicketBookingController(ICityRepository iCityRepository)
+        {
+            this.cityRepository = iCityRepository;
+        }
+        [HttpGet("City")]
+        public IActionResult City()
+        {
+            Models.City city = new Models.City();
+            city.Cities = this.cityRepository.GetCities();
+            return View(city);
+        }
+
+       
+    }
+}
